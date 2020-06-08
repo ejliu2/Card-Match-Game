7f7209d403085dc0b14a8b561b65f68520a2bb99
Eric L <ejl2@sfu.ca>
2020-06-08 13:35:39 -0700

    assignment 1 base

--- Card Match Game/ContentView.swift
+++ Card Match Game/ContentView.swift
@@ -1,21 +1,47 @@
 //
 //  ContentView.swift
-//  Card Match Game
+//  Card Match
 //
-//  Created by Eric Liu on 2020-06-08.
+//  Created by Eric Liu on 2020-06-02.
 //  Copyright © 2020 Eric Liu. All rights reserved.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    var gameType: EmojiCardMatchGame
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(gameType.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.gameType.choose(card: card)
+                }
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+        }
+}
+
+struct CardView: View {
+    var card: CardMatchGame<String>.Card
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3).foregroundColor(Color.orange).padding() // 10 point font
+                Text(card.content).padding().font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(gameType: EmojiCardMatchGame())
     }
 }

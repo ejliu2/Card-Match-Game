244e2f6aca3120f22fb58bfc87ba8e1c4f4bed8c
Eric L <ejl2@sfu.ca>
2020-06-08 13:22:31 -0700

    Initial Commit

---
+++ Card Match Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Card Match Game
+//
+//  Created by Eric Liu on 2020-06-08.
+//  Copyright © 2020 Eric Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
